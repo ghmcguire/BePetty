28e4d66bc926e9fbc4a5498cd63d5303464cf03f
Kaushika Mohan <kaushikamohan@outlook.com>
2022-10-22 23:56:50 -0400

    scrollHeader

--- script.js
+++ script.js
@@ -55,3 +55,32 @@ function sortUsers(list, field, reversed) {
 document.getElementById("login").addEventListener("click", (e) =>{
     screens.MENU.scrollIntoView({behavior: "smooth"});
 });
+
+document.getElementById("friends").addEventListener("click", (e) =>{
+  screens.CONTENT.scrollIntoView("behavior: smooth");
+  document.getElementById("screenHeader").innerHTML = "Friends List";
+});
+document.getElementById("friendReqsSent").addEventListener("click", (e) =>{
+screens.CONTENT.scrollIntoView("behavior: smooth");
+document.getElementById("screenHeader").innerHTML = "Friend Requests Sent";
+});
+document.getElementById("blockedUsers").addEventListener("click", (e) =>{
+screens.CONTENT.scrollIntoView("behavior: smooth");
+document.getElementById("screenHeader").innerHTML = "Blocked Users";
+});
+document.getElementById("deletedFriends").addEventListener("click", (e) =>{
+screens.CONTENT.scrollIntoView("behavior: smooth");
+document.getElementById("screenHeader").innerHTML = "Deleted Friends";
+});
+document.getElementById("hiddenFriends").addEventListener("click", (e) =>{
+screens.CONTENT.scrollIntoView("behavior: smooth");
+document.getElementById("screenHeader").innerHTML = "Hidden Suggestions";
+});
+document.getElementById("ignoredSnaps").addEventListener("click", (e) =>{
+screens.CONTENT.scrollIntoView("behavior: smooth");
+document.getElementById("screenHeader").innerHTML = "Ignored Snapchatters";
+});
+document.getElementById("pendingReqs").addEventListener("click", (e) =>{
+screens.CONTENT.scrollIntoView("behavior: smooth");
+document.getElementById("screenHeader").innerHTML = "Pending Requests";
+});
