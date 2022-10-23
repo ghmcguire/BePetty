74ba67951112b248d5f22f3a06700d244b38698b
Kaushika Mohan <kaushikamohan@outlook.com>
2022-10-22 23:09:34 -0400

    added menuOptions consts

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const screens = {
     "MENU": document.getElementById("menuScreen"),
     "CONTENT": document.getElementById("contentScreen")
 };
+const options = {
+    "FRIENDS": document.getElementById("friends"),
+    "SENT": document.getElementById("friendsReqsSent"),
+    "BLOCKED": document.getElementById("blockedUsers"),
+    "DELETED": document.getElementById("deletedFriends"),
+    "HIDDEN": document.getElementById("hiddenFriends"),
+    "IGNORED": document.getElementById("ignoredSnaps"),
+    "PENDING": document.getElementById("pendingReqs")
+};
 /**
  * This loads friends.json file into an object called friendsList
  */
